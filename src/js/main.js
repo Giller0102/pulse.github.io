6b5feb71bffd38cf01bdaa7b039b7244e773f8f8
Valeriya <hilevskayavaleriya@gmail.com>
2022-08-08 19:03:11 +0300

    change the stucture, change the adaptive of layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,29 @@
 const sliderBtnRight = document.querySelector('.production_slider_right');
 const sliderBtnLeft = document.querySelector('.production_slider_left');
-
+const images = document.querySelectorAll('.slider_line img');
 
 let offset = 0;
 const sliderLine = document.querySelector('.slider_line');
+let width;
+
+function init() {
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth;
+    console.log(width);
+    sliderLine.style.width = width*3 + 'px';
+
+    images.forEach(item => 
+        {
+            item.style.width = width + 'px';
+            //item.style.height = 'auto';
+        });
+}
+
+init();
+
+window.addEventListener('resize', init);
+init();
+
 
 
 sliderBtnRight.addEventListener('click', function() {
@@ -22,3 +42,8 @@ sliderBtnLeft.addEventListener('click', function() {
     }
     sliderLine.style.left = offset + 'px';
 });
+
+
+
+
+
